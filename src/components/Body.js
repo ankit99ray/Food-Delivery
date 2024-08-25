52a3e9fdc7b526fa7d2de83dfaa888bcc899a8f1
Ankit Kumar Ray <112345805+ankit99ray@users.noreply.github.com>
2024-08-25 22:05:08 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -30,7 +30,7 @@ const Body = () => {
 
     const fetchData = async () => {
         const data = await fetch(
-            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+            "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
         );
         const json = await data.json();
         // console.log(json);
