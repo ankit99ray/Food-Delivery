a8fc3c1f39bda8da2419683001dcec4657d14ef7
Ankit Kumar Ray <112345805+ankit99ray@users.noreply.github.com>
2024-08-26 21:39:13 +0530

    Update useRestaurantMenu.js

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -9,7 +9,7 @@ const useRestaurantMenu = (resId) => {
     }, []);
 
     const fetchData = async () =>{
-        const data = await fetch(MENU_API + resId);
+        const data = await fetch(MENU_API + resId, {mode: 'cors'});
         const json = await data.json();
         setResInfo(json.data);
     }
